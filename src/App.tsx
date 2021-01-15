deacff477b98b91e078da509183a6e0fe763fa36
rysh <rysher.magbanua@iselect.com.au>
2021-01-15 22:46:46 +0800

    redux dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,10 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import ProductDetailsAction from './store/actions/productDetailsAction';
 import startRootSaga from './store/rootSaga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(startRootSaga);
 
